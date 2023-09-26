490c2993d16a4f8a0013b7a48fc0c4457e30253c
IlyasOsman <IlyasOsman@users.noreply.github.com>
2023-09-26 18:12:00 +0000

    deploy: kuasakenya/kuasa-front@b36b3da0a86620fb569d7cdecd46f3d32cbba895

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -15,7 +15,7 @@ const Hero = () => (
 
       <div className="flex flex-row justify-between items-center w-full z-[5]">
         <h1 className="flex-1 font-poppins font-semibold ss:text-[72px] text-[52px] text-white ss:leading-[100.8px] leading-[75px]">
-          The <br className="sm:block hidden" /> <span className="text-gradient">KUASA</span>{" "}
+          The <br className="sm:block hidden" /> <span className="text-gradient">AEROSPACE</span>{" "}
         </h1>
         <div className="ss:flex hidden md:mr-4 mr-0">
           <GetStarted />
